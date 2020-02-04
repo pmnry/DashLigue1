0ab5f57e3aca1ec75b332dc04e0b462ed2a63785
pmnry <pierre_monroy@mfe.berkeley.edu>
2020-02-04 22:01:21 +0000

    Module to connect to an API of soccer data

---
+++ DataAcquisition.py
@@ -0,0 +1,13 @@
+import requests
+from config import API_KEY
+
+url = "https://api-football-beta.p.rapidapi.com/timezone"
+
+headers = {
+    'x-rapidapi-host': "api-football-beta.p.rapidapi.com",
+    'x-rapidapi-key': API_KEY
+    }
+
+response = requests.request("GET", url, headers=headers)
+
+print(response.text)
