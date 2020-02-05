3a03f5a88857e787ee022562b92303feedd370b0
pmnry <pierre_monroy@mfe.berkeley.edu>
2020-02-05 21:58:57 +0000

    Fix to config

--- DataAcquisition.py
+++ DataAcquisition.py
@@ -1,10 +1,10 @@
 import requests
-
+from config import API_KEY
 url = "https://api-football-beta.p.rapidapi.com/seasons"
 
 headers = {
     'x-rapidapi-host': "api-football-beta.p.rapidapi.com",
-    'x-rapidapi-key': "1101ec060fmshee8fc50f408d2b2p17f205jsn5b6b9f3764dc"
+    'x-rapidapi-key': API_KEY
     }
 
 response = requests.request("GET", url, headers=headers)
