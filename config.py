66d7345e3d61afe7f96819127c3a463efdf23f97
pmnry <pierre_monroy@mfe.berkeley.edu>
2020-05-20 14:44:43 +0100

    Update API calls

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
+from api_config import POSTGRES
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     # ...
-    SQLALCHEMY_DATABASE_URI = os.environ.get('app.db') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI =  os.environ.get('DATABASE_URL') or 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
     SQLALCHEMY_TRACK_MODIFICATIONS = False
